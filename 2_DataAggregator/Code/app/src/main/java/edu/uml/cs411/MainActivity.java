38dc869d4280776eeef7a7e3fff25ed8b4d495e9
Ian McGaunn <ian_mcgaunn@student.uml.edu>
2015-04-09 16:29:14 -0400

    Added settings activity and corresponding xml documents.

--- 2_DataAggregator/Code/app/src/main/java/edu/uml/cs411/MainActivity.java
+++ 2_DataAggregator/Code/app/src/main/java/edu/uml/cs411/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.uml.cs411;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -31,7 +32,7 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            startActivity(new Intent(this, SettingsActivity.class));
         }
 
         return super.onOptionsItemSelected(item);
